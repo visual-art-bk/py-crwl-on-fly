0b3487d65fc8dc7e224b8668de912c185f571522
kbk <kekekj@naver.com>
2024-10-28 18:04:17 +0900

    success google chrome siple crawling

--- core/routes.py
+++ core/routes.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, request, jsonify, render_template_string
-from core.selenium_utils import WebDriverSingleton
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 import traceback
 
 # 블루프린트 생성
@@ -10,12 +12,22 @@ main = Blueprint("main", __name__)
 @main.route("/")
 def home():
     try:
-        driver = WebDriverSingleton.get_driver()  # 싱글턴 driver 인스턴스 가져오기
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")  # GUI 없이 실행
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+
+
+        driver_path = "/app/static/drivers/chromedriver/chromedriver"
+        
+        service = Service(driver_path)
+        driver = webdriver.Chrome(service=service, options=chrome_options)
+        
         driver.get("https://google.com")
         data = driver.title  # 예시로 페이지 제목 가져오기
 
         return f"<h1>{data}</h1>"
-    
+
     except Exception as e:
         # 예외 발생 시 HTML로 출력
         error_message = traceback.format_exc()

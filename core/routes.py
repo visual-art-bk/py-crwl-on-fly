70dea7fce8ed6fea7b071aee59a47cc65c06eb6e
kbk <kekekj@naver.com>
2024-10-29 08:55:18 +0900

    Before of modifying abs path of static

--- core/routes.py
+++ core/routes.py
@@ -1,6 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template_string
 import traceback
 from core.utils.selenium_utils import CrawlingWebDriver
+from selenium.common.exceptions import WebDriverException
 
 # 블루프린트 생성
 main = Blueprint("main", __name__)
@@ -12,10 +13,15 @@ def home():
         # 사용 예제
         with CrawlingWebDriver() as driver:
             driver.get("https://google.com")
+
             print(driver.title)  # 페이지 타이틀 출력
             data = driver.title  # 예시로 페이지 제목 가져오기
         return f"<h1>{data} - CrawlingWebDriver Test OK </h1>"
-
+            # driver.quit() 후 세션 종료 여부를 확인하기 위해 Selenium 메서드 호출
+            
+    # except WebDriverException as e:
+    #     return f"<h1>{e}</h1>"
+        
     except Exception as e:
         # 예외 발생 시 HTML로 출력
         error_message = traceback.format_exc()

0d05eb12368e3a09930ef5f6454e8e3a8888f019
kbk <kekekj@naver.com>
2024-10-29 10:45:24 +0900

    change routes.py to /core/coutes/routes.py for which many route handlers

--- core/__init__.py
+++ core/__init__.py
@@ -4,7 +4,7 @@ def create_app():
     app = Flask(__name__)
 
     # 라우팅 모듈을 import하고 등록
-    from core.routes import main
+    from core.routes.routes import main
     app.register_blueprint(main)
 
     return app

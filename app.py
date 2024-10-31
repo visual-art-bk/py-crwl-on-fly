b98b1015f1476bcb639cfd9cc54643ebbb66d12d
kbk <kekekj@naver.com>
2024-10-31 11:03:02 +0900

    feat: Add chang PORT dinamically with hosting
    - PORT 5000 => Heroku
    - PORT 8000 => Fly.io

--- app.py
+++ app.py
@@ -8,5 +8,9 @@ app = create_app()
 
 
 if __name__ == "__main__":
+
+    # DYNAMIC PORT
+    # - Hero PORT   - 5000, THE DEFAULT
+    # - Fly.io PORT - 8080, SEE ./fly.toml CHECK [env] - PORT
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)

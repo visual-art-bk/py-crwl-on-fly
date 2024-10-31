b788f22ef08b8982639fd6ab985b6ec58ff1507f
kbk <kekekj@naver.com>
2024-10-31 22:12:59 +0900

    refactor: change driver to manager
    - changed the instance of CrawlingWebDriver to manager which have props. not drive.

--- core/route_handlers/home.py
+++ core/route_handlers/home.py
@@ -3,13 +3,13 @@ from core.exceptions.route_exceptions import RouteHandlerError
 
 def home_handler():
     try:
-        with CrawlingWebDriver() as driver:
-            driver.get("https://google.com")
+        with CrawlingWebDriver() as manager:
+            manager.driver.get("https://google.com")
 
-            print(driver.title)  
-            data = driver.title  
+            print(manager.driver.title)  
+            data = manager.driver.title  
             
-        return f"<h1>{data} - CrawlingWebDriver Test OK </h1>"
+        return f"<h1>{data} - CrawlingWebDriver Test OK 123 </h1>"
 
     except Exception as e:
         raise RouteHandlerError(e)

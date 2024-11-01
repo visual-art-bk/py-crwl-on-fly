7418ab274488f3413fabeb272348d6d487ac1de7
kbk <kekekj@naver.com>
2024-11-01 12:22:53 +0900

    move out handler

--- core/blueprints/bp_routes.py
+++ core/blueprints/bp_routes.py
@@ -1,11 +1,18 @@
 from flask import Blueprint
 from core.utils.WebScrpDriverManager import WebScrpDriverManager
 from core.route_handlers.home import home_handler
-from core.route_handlers.scr_nv_blog import NvBlogScraper, render_tmp_html
+from core.route_handlers.scr_nv_blog import (
+    NvBlogScraper,
+    render_tmp_html,
+    handler_search_naver_blog,
+)
 from core.exceptions.route_exceptions import RouteHandlerError
 import time
+from core.utils.decorate_testers import test_simulate_multiple_requests
+from core.utils.testers import request_tester
 
 main = Blueprint("main", __name__)
+
 
 @main.route("/")
 def home():
@@ -13,20 +20,18 @@ def home():
 
 
 @main.route("/scr/nv-blog")
+@test_simulate_multiple_requests(request_count=5, delay=0.2)
 def _():
+
     try:
         # with문이 종료되면 driver.quit() 실행되므로, 추가 콜 필요없음
         with WebScrpDriverManager() as manager:
             manager.inspect_request_count()
-            
-            nv_blg_scrper = NvBlogScraper()
-            
-            nv_blg_scrper.init(manager.driver)
-            
-            nv_blg_scrper.open_browser("https://www.naver.com")
-            nv_blg_scrper.search_keyword("강아지 사료", "input[name='query']")
-            
-            return nv_blg_scrper.render_test_html()
+
+            return request_tester(manager.driver)
+
+            # return handler_search_naver_blog(manager)
+
     except Exception as e:
         raise RouteHandlerError(e)
     finally:
--- core/route_handlers/scr_nv_blog.py
+++ core/route_handlers/scr_nv_blog.py
@@ -4,12 +4,23 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from core.exceptions.route_exceptions import RouteHandlerError
 from core.utils.WebScarper import WebScarper
+from core.utils import WebScrpDriverManager
+
 
 def render_tmp_html():
     return "<h1>네이버 블로그 스크래핑 시작123<h1>"
 
 
 class NvBlogScraper(WebScarper):
-    
-
     pass
+
+
+def handler_search_naver_blog(manager: WebScrpDriverManager):
+    url = "https://www.naver.com"
+    nv_blg_scrper = NvBlogScraper()
+
+    nv_blg_scrper.init(manager.driver)
+    nv_blg_scrper.open_browser(url)
+    nv_blg_scrper.search_keyword("강아지 사료", "input[name='query']")
+
+    return nv_blg_scrper.render_test_html()

b65ab3f2efd17565473abba97f8cbea759c0a1ca
kbk <kekekj@naver.com>
2024-11-01 13:55:22 +0900

    refactor

--- core/blueprints/bp_routes.py
+++ core/blueprints/bp_routes.py
@@ -26,10 +26,7 @@ def _():
     try:
         # with문이 종료되면 driver.quit() 실행되므로, 추가 콜 필요없음
         with WebScrpDriverManager() as manager:
-
-            return request_tester(manager.driver)
-
-            # return handler_search_naver_blog(manager)
+            return handler_search_naver_blog(manager)
 
     except Exception as e:
         raise RouteHandlerError(e)
